bdfb3c91e08012c3e982600b43baca40510c3fd3
Bruno Pascutti <brupascutti@gmail.com>
2024-03-11 11:49:17 -0300

    heran

---
+++ Heran.ex/Entities/Company.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heran.ex.Entities
+{
+    internal class Company : TaxPayer
+    {
+        public int NumeberOfEmployees { get; set; }
+
+        public Company() { }
+
+        public Company(string name, double anualIncome, int numeberOfEmployees) : base(name, numeberOfEmployees)
+        {
+            NumeberOfEmployees = numeberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            if (NumeberOfEmployees > 10)
+            {
+                return AnualIncome * 0.14;
+            }
+            else
+            {
+                return AnualIncome * 0.16;
+            }
+        }
+    }
+}
